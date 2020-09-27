a2c01e0056d4b59bcc78dce16c8aafcbb7a9e050
Wesley <wesley.contato@hotmail.com>
2020-09-26 21:43:16 -0300

    Revisão e a classe Funcionario

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,17 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("Nico Steppat");
+		nico.setCpf("223325545-87");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+
+	}
+
+}
