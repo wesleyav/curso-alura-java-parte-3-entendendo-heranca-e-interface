7229cccaf751266ba5c7d22e744b9d99f43d0a97
Wesley <wesley.contato@hotmail.com>
2020-09-28 07:11:29 -0300

    Aplicando Polimorfismo

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,8 @@
+
+public class EditorVideo extends Funcionario {
+
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificação do EDITOR DE VIDEO");
+		return super.getBonificacao() + 100;
+	}
+}
--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -3,12 +3,23 @@ public class TesteReferencias {
 
 	public static void main(String[] args) {
 		
-		Funcionario g1 = new Gerente();
+		Gerente g1 = new Gerente();
 		g1.setNome("Marcos");
-		String nome = g1.getNome();
+		g1.setSalario(5000.0);
 		
-		System.out.println(nome);
-
+		Funcionario f = new Funcionario();
+		f.setSalario(2000.0);
+		
+		EditorVideo ev = new EditorVideo();
+		f.setSalario(2500.0);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f);
+		controle.registra(ev);
+		
+		System.out.println(controle.getSoma());
+		
 	}
 
 }
