10aa40a94ef5f9df95a40740bd7168f719f0c246
Wesley <wesley.contato@hotmail.com>
2020-09-28 05:23:30 -0300

    Tipos de funcionários

---
+++ bytebank-herdado/src/FuncionarioTeste.java
@@ -0,0 +1,57 @@
+
+public class FuncionarioTeste {
+
+	private String nome;
+	private String cpf;
+	private double salario;
+	private int tipo = 0; // 0 - Funcionario comum, 1 = Gerente, 2 - Diretor
+	
+	public FuncionarioTeste() {
+
+	}
+
+	public double getBonificacao() {
+
+		if (this.tipo == 0) {// 0 - Funcionario comum
+			return this.salario * 0.1;
+		} else if (this.tipo == 1) { // 1 - Funcionario Gerente
+			return this.salario;
+		} else {
+			return this.salario + 1000.00;
+		}
+
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+}
