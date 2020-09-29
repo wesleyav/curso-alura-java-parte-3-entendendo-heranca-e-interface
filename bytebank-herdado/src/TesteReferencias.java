fe3806c4faf55c7ba9b5a506dce0e52bcfa89ee0
Wesley <wesley.contato@hotmail.com>
2020-09-29 05:49:48 -0300

    Classes abstratas

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -6,19 +6,15 @@ public class TesteReferencias {
 		Gerente g1 = new Gerente();
 		g1.setNome("Marcos");
 		g1.setSalario(5000.0);
-		
-		Funcionario f = new Funcionario();
-		f.setSalario(2000.0);
-		
+			
 		EditorVideo ev = new EditorVideo();
-		f.setSalario(2500.0);
+		ev.setSalario(2500.0);
 		
 		Designer d = new Designer();
-		f.setSalario(2000.0);
+		d.setSalario(2000.0);
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
-		controle.registra(f);
 		controle.registra(ev);
 		controle.registra(d);
 		
